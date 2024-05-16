fa21b35f0d730bbcac45b4ac639e1bdeb0a3700b
Li Tao <aeolia.zafiro@gmail.com>
2024-05-17 00:40:41 +0800

    optimal performance
    optimal performance

--- ATCG/sortRow.hpp
+++ ATCG/sortRow.hpp
@@ -213,6 +213,74 @@ void sortRow(Array<double, 2> &cell_array, Array<double, 2> cell_array1, int Col
                     break;
                 }
                     
+                case 32:
+                {
+                    Range all = Range::all();
+                    int C_16= cell_array.rows();
+                    struct array{
+                        double data;
+                        int index;
+                    };
+                    
+                    array *p=new array[C_16];
+                    
+                    
+                    for (int CN=0; CN<C_16; CN++)
+                    {
+                        p[CN].data = cell_array(CN+1,colnum_to_sort);
+                        p[CN].index = CN+1;
+                    }
+                    
+                    qsort(p,C_16,sizeof(p[0]),cmp1);
+                    
+                    cell_array1.resize(C_16, Col);
+                    
+#pragma omp parallel for schedule(static)
+                    {
+                        for (int CNx=0; CNx<C_16; CNx++)
+                        {
+                            cell_array1(CNx+1,1)=cell_array(p[CNx].index,1);
+                            cell_array1(CNx+1,2)=cell_array(p[CNx].index,2);
+                            cell_array1(CNx+1,3)=cell_array(p[CNx].index,3);
+                            cell_array1(CNx+1,4)=cell_array(p[CNx].index,4);
+                            cell_array1(CNx+1,5)=cell_array(p[CNx].index,5);
+                            cell_array1(CNx+1,6)=cell_array(p[CNx].index,6);
+                            cell_array1(CNx+1,7)=cell_array(p[CNx].index,7);
+                            cell_array1(CNx+1,8)=cell_array(p[CNx].index,8);
+                            cell_array1(CNx+1,9)=cell_array(p[CNx].index,9);
+                            cell_array1(CNx+1,10)=cell_array(p[CNx].index,10);
+                            cell_array1(CNx+1,11)=cell_array(p[CNx].index,11);
+                            cell_array1(CNx+1,12)=cell_array(p[CNx].index,12);
+                            cell_array1(CNx+1,13)=cell_array(p[CNx].index,13);
+                            cell_array1(CNx+1,14)=cell_array(p[CNx].index,14);
+                            cell_array1(CNx+1,15)=cell_array(p[CNx].index,15);
+                            cell_array1(CNx+1,16)=cell_array(p[CNx].index,16);
+                            cell_array1(CNx+1,17)=cell_array(p[CNx].index,17);
+                            cell_array1(CNx+1,18)=cell_array(p[CNx].index,18);
+                            cell_array1(CNx+1,19)=cell_array(p[CNx].index,19);
+                            cell_array1(CNx+1,20)=cell_array(p[CNx].index,20);
+                            cell_array1(CNx+1,21)=cell_array(p[CNx].index,21);
+                            cell_array1(CNx+1,22)=cell_array(p[CNx].index,22);
+                            cell_array1(CNx+1,23)=cell_array(p[CNx].index,23);
+                            cell_array1(CNx+1,24)=cell_array(p[CNx].index,24);
+                            cell_array1(CNx+1,25)=cell_array(p[CNx].index,25);
+                            cell_array1(CNx+1,26)=cell_array(p[CNx].index,26);
+                            cell_array1(CNx+1,27)=cell_array(p[CNx].index,27);
+                            cell_array1(CNx+1,28)=cell_array(p[CNx].index,28);
+                            cell_array1(CNx+1,29)=cell_array(p[CNx].index,29);
+                            cell_array1(CNx+1,30)=cell_array(p[CNx].index,30);
+                            cell_array1(CNx+1,31)=cell_array(p[CNx].index,31);
+                            cell_array1(CNx+1,32)=cell_array(p[CNx].index,32);
+                        }
+                    }
+                    
+                    cell_array(all,all)=0;
+                    cell_array(all,all)=cell_array1(all,all);
+                    delete[] p;
+                    p=NULL;
+                    break;
+                }
+                    
             }
             break;
         }
