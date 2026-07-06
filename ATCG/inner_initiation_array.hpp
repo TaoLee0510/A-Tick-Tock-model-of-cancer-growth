34c4f7e15149cc4fb0c0064fd33e7eaddd0610f1
Li Tao <aeolia.zafiro@gmail.com>
2026-07-06 19:57:36 -0400

    Add CellStore initialization entry points

--- ATCG/inner_initiation_array.hpp
+++ ATCG/inner_initiation_array.hpp
@@ -32,8 +32,9 @@
 #include <blitz/array.h>
 #include "random_uniform.hpp"
 #include "stateless_rng.hpp"
+#include "cell_store.hpp"
 using namespace blitz;
-void inner_initiation_array(int N0,int N01,int R0,int Visual_range_x, int Visual_range_y, Array<double,2> &cell_array_inner, Array<long,3> Visual_range, double uniup_r1, double unilow_r1, double sigmahatr,double muhatr, double uniup_K1, double unilow_K1, double sigmahatK,double muhatK, int N0r1,int N0K1, double *migration_rate_r1, double *migration_rate_K1)
+void inner_initiation_array(int N0,int N01,int R0,int Visual_range_x, int Visual_range_y, Array<double,2> &cell_array_inner, const Array<long,3> &Visual_range, double uniup_r1, double unilow_r1, double sigmahatr,double muhatr, double uniup_K1, double unilow_K1, double sigmahatK,double muhatK, int N0r1,int N0K1, double *migration_rate_r1, double *migration_rate_K1)
 {
     const long rng_context = 10003;
     double initial_r_growth_rate[N0r1];
@@ -120,6 +121,14 @@ void inner_initiation_array(int N0,int N01,int R0,int Visual_range_x, int Visual
             a=a+1;
         }
     }
+}
+
+inline CellStore inner_initiation_cell_store(int N0,int N01,int R0,int Visual_range_x, int Visual_range_y, const Array<long,3> &Visual_range, double uniup_r1, double unilow_r1, double sigmahatr,double muhatr, double uniup_K1, double unilow_K1, double sigmahatK,double muhatK, int N0r1,int N0K1, double *migration_rate_r1, double *migration_rate_K1, int Col)
+{
+    Array<double,2> cell_array_inner(N01, Col, FortranArray<2>());
+    cell_array_inner = 0;
+    inner_initiation_array(N0, N01, R0, Visual_range_x, Visual_range_y, cell_array_inner, Visual_range, uniup_r1, unilow_r1, sigmahatr, muhatr, uniup_K1, unilow_K1, sigmahatK, muhatK, N0r1, N0K1, migration_rate_r1, migration_rate_K1);
+    return cell_store_from_array(cell_array_inner, Col);
 }
 
 #endif /* inner_initiation_array_hpp */
--- ATCG/outer_initiation_array_low_density.hpp
+++ ATCG/outer_initiation_array_low_density.hpp
@@ -32,9 +32,10 @@
 #include <blitz/array.h>
 #include "random_uniform.hpp"
 #include "stateless_rng.hpp"
+#include "cell_store.hpp"
 using namespace blitz;
 
-Array<double,2> outer_initiation_array_low_density(int N0, int Visual_range_x, int Visual_range_y, Array<int,2> A, double uniup_r, double unilow_r, double sigmahatr,double muhatr, double uniup_K, double unilow_K, double sigmahatK,double muhatK, int N0r,int N0K, double *migration_rate_r, double *migration_rate_K)
+Array<double,2> outer_initiation_array_low_density(int N0, int Visual_range_x, int Visual_range_y, const Array<int,2> &A, double uniup_r, double unilow_r, double sigmahatr,double muhatr, double uniup_K, double unilow_K, double sigmahatK,double muhatK, int N0r,int N0K, double *migration_rate_r, double *migration_rate_K)
 {
     const long rng_context = 10002;
     double initial_r_growth_rate[N0r];
@@ -146,6 +147,12 @@ Array<double,2> outer_initiation_array_low_density(int N0, int Visual_range_x, i
         }
     }
     return cell_array_out_1;
+}
+
+inline CellStore outer_initiation_low_density_cell_store(int N0, int Visual_range_x, int Visual_range_y, const Array<int,2> &A, double uniup_r, double unilow_r, double sigmahatr,double muhatr, double uniup_K, double unilow_K, double sigmahatK,double muhatK, int N0r,int N0K, double *migration_rate_r, double *migration_rate_K)
+{
+    Array<double,2> cell_array_out = outer_initiation_array_low_density(N0, Visual_range_x, Visual_range_y, A, uniup_r, unilow_r, sigmahatr, muhatr, uniup_K, unilow_K, sigmahatK, muhatK, N0r, N0K, migration_rate_r, migration_rate_K);
+    return cell_store_from_array(cell_array_out, cell_col::kStandardColumnCount);
 }
 
 #endif /* outer_initiation_array_low_density_hpp */
