fa2f24a8a5c340400775793c68221974f5a044ba
Li Tao <aeolia.zafiro@gmail.com>
2026-07-06 22:19:37 -0400

    Remove visual range matrix compatibility

--- ATCG/visual_range.hpp
+++ ATCG/visual_range.hpp
@@ -9,7 +9,6 @@
 #include <algorithm>
 #include <cstddef>
 #include <vector>
-#include <blitz/range.h>
 
 class VisualRange
 {
@@ -43,16 +42,6 @@ public:
     int width() const { return width_; }
     int height() const { return height_; }
 
-    long &operator()(int x, int y, int layer)
-    {
-        return layer_ref(x, y, layer);
-    }
-
-    long operator()(int x, int y, int layer) const
-    {
-        return layer_value(x, y, layer);
-    }
-
     long &occupied(int x, int y) { return occupied_[index(x, y)]; }
     long occupied(int x, int y) const { return occupied_[index(x, y)]; }
 
@@ -77,10 +66,10 @@ public:
 
     void write_site(int x, int y, long cell_id, long cell_stage, long label)
     {
-        (*this)(x, y, 1) = 1;
-        (*this)(x, y, 2) = cell_id;
-        (*this)(x, y, 3) = cell_stage;
-        (*this)(x, y, 4) = label;
+        occupied(x, y) = 1;
+        density_label(x, y) = cell_id;
+        stage(x, y) = cell_stage;
+        cell_label(x, y) = label;
     }
 
     void write_square(int x1, int y1, long cell_id, long cell_stage, long label)
@@ -111,102 +100,29 @@ public:
         set_rect(x1, x1 + 1, y1, y1 + 1, 4, 4, value);
     }
 
-    class SiteLayersProxy
-    {
-    public:
-        SiteLayersProxy(VisualRange &range, int x, int y, int first_layer, int last_layer)
-            : range_(range), x_(x), y_(y), first_layer_(first_layer), last_layer_(last_layer)
-        {
-        }
-
-        SiteLayersProxy &operator=(long value)
-        {
-            range_.set_layers(x_, y_, first_layer_, last_layer_, value);
-            return *this;
-        }
-
-    private:
-        VisualRange &range_;
-        int x_;
-        int y_;
-        int first_layer_;
-        int last_layer_;
-    };
-
-    class RectLayersProxy
-    {
-    public:
-        RectLayersProxy(VisualRange &range, int first_x, int last_x, int first_y, int last_y, int first_layer, int last_layer)
-            : range_(range), first_x_(first_x), last_x_(last_x), first_y_(first_y), last_y_(last_y), first_layer_(first_layer), last_layer_(last_layer)
-        {
-        }
-
-        RectLayersProxy &operator=(long value)
-        {
-            range_.set_rect(first_x_, last_x_, first_y_, last_y_, first_layer_, last_layer_, value);
-            return *this;
-        }
-
-    private:
-        VisualRange &range_;
-        int first_x_;
-        int last_x_;
-        int first_y_;
-        int last_y_;
-        int first_layer_;
-        int last_layer_;
-    };
-
-    SiteLayersProxy operator()(int x, int y, const blitz::Range &layers)
-    {
-        return SiteLayersProxy(*this, x, y, layers.first(1), layers.last(4));
-    }
-
-    RectLayersProxy operator()(const blitz::Range &xs, const blitz::Range &ys, int layer)
-    {
-        return RectLayersProxy(*this, xs.first(1), xs.last(width_), ys.first(1), ys.last(height_), layer, layer);
-    }
-
-    RectLayersProxy operator()(const blitz::Range &xs, const blitz::Range &ys, const blitz::Range &layers)
-    {
-        return RectLayersProxy(*this, xs.first(1), xs.last(width_), ys.first(1), ys.last(height_), layers.first(1), layers.last(4));
-    }
-
 private:
     std::size_t index(int x, int y) const
     {
         return static_cast<std::size_t>(x - 1) * static_cast<std::size_t>(height_) + static_cast<std::size_t>(y - 1);
     }
 
-    long &layer_ref(int x, int y, int layer)
+    void set_layer(int x, int y, int layer, long value)
     {
         std::size_t site = index(x, y);
         switch (layer)
         {
             case 1:
-                return occupied_[site];
+                occupied_[site] = value;
+                break;
             case 2:
-                return density_label_[site];
+                density_label_[site] = value;
+                break;
             case 3:
-                return stage_[site];
+                stage_[site] = value;
+                break;
             default:
-                return cell_label_[site];
-        }
-    }
-
-    long layer_value(int x, int y, int layer) const
-    {
-        std::size_t site = index(x, y);
-        switch (layer)
-        {
-            case 1:
-                return occupied_[site];
-            case 2:
-                return density_label_[site];
-            case 3:
-                return stage_[site];
-            default:
-                return cell_label_[site];
+                cell_label_[site] = value;
+                break;
         }
     }
 
@@ -214,7 +130,7 @@ private:
     {
         for (int layer = first_layer; layer <= last_layer; ++layer)
         {
-            (*this)(x, y, layer) = value;
+            set_layer(x, y, layer, value);
         }
     }
 
