6fbe6ec08275349a283da8652943dd1544d5bf43
Li Tao <aeolia.zafiro@gmail.com>
2026-07-06 20:06:50 -0400

    Avoid death judgement subvisual copy

--- ATCG/death_judgement.hpp
+++ ATCG/death_judgement.hpp
@@ -19,7 +19,7 @@
 #include <blitz/array.h>
 #include "stateless_rng.hpp"
 using namespace blitz;
-void death_judgement(int Visual_range_x, int Visual_range_y, int N00, int N01, double r_limit, double K_limit, double lambda_r, double lambda_K, double alpha, double beta, double carrying_capacity_r, double carrying_capacity_K, double Cr, double CK, double death_time_range_r, double death_time_range_K, double deltah, double &h, Array<double, 2> &cell_array, Array<double,2> &cell_array_temp, Array<long, 3> sub_visual, Array<long,3> &Visual_range, double deathjudge, int Col,int nthreads,long rng_time_step)
+void death_judgement(int Visual_range_x, int Visual_range_y, int N00, int N01, double r_limit, double K_limit, double lambda_r, double lambda_K, double alpha, double beta, double carrying_capacity_r, double carrying_capacity_K, double Cr, double CK, double death_time_range_r, double death_time_range_K, double deltah, double &h, Array<double, 2> &cell_array, Array<double,2> &cell_array_temp, Array<long, 3> &sub_visual, Array<long,3> &Visual_range, double deathjudge, int Col,int nthreads,long rng_time_step)
 {
     Range all = Range::all();
     int C= cell_array.rows();
